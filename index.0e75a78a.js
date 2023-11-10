06cb0109864935afd3258b21305062cf2647af07
Oleksii03 <102686871+Oleksii03@users.noreply.github.com>
2023-11-10 14:39:22 +0000

    Deploying to gh-pages from @ Oleksii03/gym-template@72a59692481de4a8637f845b6d5fcd96738bd9a4 🚀

---
+++ index.0e75a78a.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".js-menu__btn"),n=document.querySelector(".js-mob-menu");e.addEventListener("click",(function(e){n.classList.toggle("is-visible")}))}();
+//# sourceMappingURL=index.0e75a78a.js.map
---
+++ index.e73d2a65.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".js-menu__btn"),t=document.querySelector(".js-mob-menu");e.addEventListener("click",(function(e){t.classList.toggle("is-visible")}));
+//# sourceMappingURL=index.e73d2a65.js.map
